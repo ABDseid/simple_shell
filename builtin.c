937a989293f0f9d153dcb7c0bd1cea937e2d6bfe
ABDseid <abddseid@gmail.com>
2023-02-22 12:52:35 -0800

    updated simple_shell

--- builtin.c
+++ builtin.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
@@ -9,17 +8,16 @@
  */
 int _myexit(info_t *info)
 {
-	int exitcheck;
-
-	if (info->argv[1]) /* If there is an exit arguement */
-	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
+int exitcheck;
+if (info->argv[1]) /* If there is an exit arguement */
+{
+exitcheck = _erratoi(info->argv[1]);
+if (exitcheck == -1)
+{
+info->status = 2;
+print_error(info, "Illegal number: ");
+_eputs(info->argv[1]);
+_eputchar('\n');
 return (1);
 }	
 info->err_num = _erratoi(info->argv[1]);
@@ -28,7 +26,6 @@ return (-2);
 info->err_num = -1;
 return (-2);
 }
-
 /**
  * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
